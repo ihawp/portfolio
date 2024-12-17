c951821dbc11bb2e50da88e2082b126f8fed94b3
ihawp <ihawpebay@gmail.com>
2024-12-16 20:01:52 -0600

    updated error page to be green because funny

--- src/page/Error404.tsx
+++ src/page/Error404.tsx
@@ -2,10 +2,10 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 export default function Error404() {
     return <div className={"w-full h-[300px] flex justify-center items-center"}>
-        <ul className={"list-none rounded bg-red-500 p-5 flex flex-col items-center justify-center"}>
+        <ul className={"list-none rounded bg-green-600 p-5 gap-2 flex flex-col items-center justify-center"}>
 
             <FontAwesomeIcon icon={"circle-exclamation"} className={"text-7xl"}/>
-            <h1 className={"text-5xl font-bold bg-red-500 p-4 rounded"}>404 Error</h1>
+            <h1 className={"text-5xl font-bold"}>404 Error</h1>
             <p className={"text-2xl"}>Page not found.</p>
 
         </ul>
